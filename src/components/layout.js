11a36f0b034179942194cf96bbd8f33c8e22b579
Brunofarias <brunofarias17@hotmail.com>
2024-11-11 10:29:43 -0400

    Reconectando no Git

---
+++ src/components/layout.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import "./layout.css";
+import Footer from "./footer";
+
+export default function Layout({ children }) {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <main className="layout">
+      <div className="header">
+        <h3>{data.site.siteMetadata.title}</h3>
+        <nav className="topnav">
+          <Link to="/">Início</Link>
+        </nav>
+      </div>
+      <div className="main">{children}</div>
+      <Footer copyrightYear="2024" />
+    </main>
+  );
+}
